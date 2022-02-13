8dad2a68ea937a302edb07a8c3f85720e9b43093
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-13 22:08:54 +0530

    modified App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,25 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Avatar img={contacts[0].imgURL} />
-      <Card
+      {/* <Avatar img={contacts[0].imgURL} /> */}
+      {contacts.map(createCard)}
+      {/* <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
         tel={contacts[0].phone}
@@ -31,7 +44,7 @@ function App() {
         img={contacts[3].imgURL}
         tel={contacts[3].phone}
         email={contacts[3].email}
-      />
+      /> */}
     </div>
   );
 }
