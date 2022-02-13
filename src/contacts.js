f2ce6ce28fbe1b5657abce0a6db697747c5ab494
SinhaVinit <vinit.8444935532@gmail.com>
2022-02-13 21:35:45 +0530

    Added App.jsx Card.jsx contacts.js and modified styles.css index.js

---
+++ src/contacts.js
@@ -0,0 +1,32 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Vinit Sinha",
+    imgURL: "https://upload.wikimedia.org/wikipedia/tr/thumb/3/30/NarutoUzumaki.jpg/300px-NarutoUzumaki.jpg",
+    phone: "+91 0123456789",
+    email: "vinit@mail.com"
+  },
+  {
+    id: 2,
+    name: "Vishal Sinha",
+    imgURL: "https://community.custom-cursor.com/uploads/default/original/3X/a/4/a4ec3bdb0d1a3db93978c977b3e8d87158a8c440.jpeg",
+    phone: "+91 1234567890",
+    email: "vishal@mail.com"
+  },
+  {
+    id: 3,
+    name: "Shikamaru Nara",
+    imgURL: "https://i.redd.it/42f5dmfqzt661.jpg",
+    phone: "+91 2345678901",
+    email: "shikamaru@mail.com"
+  },
+  {
+    id: 4,
+    name: "Neji Hyūga",
+    imgURL: "http://images5.fanpop.com/image/photos/27100000/neji-anime-naruto-all-character-27190270-640-480.jpg",
+    phone: "+91 3456789012",
+    email: "neji@mail.com"
+  }
+];
+
+export default contacts;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name="Vinit Sinha"
-      img="https://upload.wikimedia.org/wikipedia/tr/thumb/3/30/NarutoUzumaki.jpg/300px-NarutoUzumaki.jpg"
-      tel="+91 1234567890"
-      email="vinit@mail.com"
-    />
-    <Card
-      name="Vishal Sinha"
-      img="https://community.custom-cursor.com/uploads/default/original/3X/a/4/a4ec3bdb0d1a3db93978c977b3e8d87158a8c440.jpeg"
-      tel="+91 0123456789"
-      email="vishal@mail.com"
-    />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
